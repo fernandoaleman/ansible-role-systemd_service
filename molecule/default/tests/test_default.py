e3496648d966db4d6051c5cb838e9d9f61b01cee
Fernando Alemán <fernandoaleman@mac.com>
2025-05-21 11:43:04 -0500

    Ensure systemd service enabled

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,8 +1,7 @@
 # molecule/default/tests/test_default.py
+systemd_service_name = "example"
 
 
-def test_host_is_reachable(host):
-    """Ensure etc directory exists"""
-    etc = host.file("/etc")
-    assert etc.exists
-    assert etc.is_directory
+def test_service_is_enabled(host):
+    svc = host.service(systemd_service_name)
+    assert svc.is_enabled
