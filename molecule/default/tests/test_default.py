091e6b69932f227205a0a59cfde59ee2a06322b8
Fernando Alemán <fernandoaleman@mac.com>
2025-05-21 11:47:22 -0500

    Ensure systemd service started

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,3 +5,8 @@ systemd_service_name = "example"
 def test_service_is_enabled(host):
     svc = host.service(systemd_service_name)
     assert svc.is_enabled
+
+
+def test_service_is_running(host):
+    svc = host.service(systemd_service_name)
+    assert svc.is_running
